bd75b2bcbf453fadcce571f87cbf4125847afc5f
Bohdan Sushchak <b.sushchak@ideo.pl>
2020-01-03 12:14:28 +0100

    text styles updated

--- lib/utils/config/TextStyles.dart
+++ lib/utils/config/TextStyles.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:forecast_flutter/utils/config/AppColors.dart';
 
 abstract class TextStyles {
-  static TextStyle logo = TextStyle(fontSize: 28.0, fontWeight: FontWeight.normal, color: AppColors.black);
-  static TextStyle tempMain = TextStyle(fontSize: 34.0, fontWeight: FontWeight.bold, color: AppColors.white);
+  static TextStyle whiteBig = TextStyle(fontSize: 28.0, fontWeight: FontWeight.normal, color: AppColors.white);
+
+  static TextStyle tempMain = TextStyle(fontSize: 36.0, fontWeight: FontWeight.bold, color: AppColors.white);
 }
